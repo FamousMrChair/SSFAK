bd716fa4784a1e96fb599a77ebdf5792bab0c4e8
FamousMrChair <kxiao30@stuy.edu>
2022-11-03 09:26:16 -0400

    setup init

---
+++ 19_session/app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)    #create Flask object
+
+
+@app.route("/", methods = ['GET', 'POST'])
+def disp_loginpage():
+    print("\n\n\n")
+    print("***DIAG: this Flask obj ***")
+    print(app)
+    print("***DIAG: request obj ***")
+    print(request)
+    print("***DIAG: request.args ***")
+    print(request.args)
+    #print("***DIAG: request.args['username']  ***")
+    #print(request.args['username'])
+    print("***DIAG: request.headers ***")
+    print(request.headers)
+    return render_template( 'login.html' )
+
+
+@app.route("/auth", methods = ['GET', 'POST'])
+def authenticate():
+    print("\n\n\n")
+    print("***DIAG: this Flask obj ***")
+    print(app)
+    print("***DIAG: request obj ***")
+    print(request)
+    print("***DIAG: request.args ***")
+    print(request.args)
+    #print("***DIAG: request.args['username']  ***")
+    #print(request.args['username'])
+    print("***DIAG: request.headers ***")
+    print(request.headers)
+    if (request.form['username'] == 'GIRLBOSS' and request.form['password'] == 'slay'):
+        return render_template('response.html', username= request.form["username"])  #response to a form submission
+    else:
+        return render_template('bad.html')
+
+    
+
+if __name__ == "__main__": #false if this file imported as module
+    #enable debugging, auto-restarting of server when this file is modified
+    app.debug = True 
+    app.run()
