1f8c1149be943f8294ca84373a8725f22c11ff3b
FamousMrChair <kxiao30@stuy.edu>
2022-11-21 20:35:05 -0500

    done

--- 20_restapi/app.py
+++ 20_restapi/app.py
@@ -1,4 +1,5 @@
-
+from urllib.request import urlopen
+import json
 
 from flask import Flask, render_template, request
 from flask import session, redirect, url_for
@@ -7,8 +8,23 @@ from flask import session, redirect, url_for
 app = Flask(__name__)    #create Flask object
 #FUTURE PLANS MAKE SURE TO CHECK IF FORMS ARE VALID
 
+myfile = open("key_nasa.txt", "rt") # open key_nasa.txt for reading text
+key = myfile.read()         # read the entire file to string
+myfile.close()                   # close the file
 
+api = "https://api.nasa.gov/planetary/apod?api_key=" + key
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template("main.html")
+    call = urlopen(api)
+    data_json = json.loads(call.read())
+    url = data_json["url"]
+    explanation = data_json["explanation"]
+    print (url)
+    print (data_json)
+    return render_template("main.html", url=url, explanation = explanation)
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
