608bcc60c8860f9b8ec2e83275c4e53396cf8e9f
FamousMrChair <kxiao30@stuy.edu>
2023-04-04 19:58:47 -0400

    27_js

---
+++ 27_js/fibfac.js
@@ -0,0 +1,13 @@
+// Team Phantom Tollbooth :: Kevin Xiao, Aden Garbutt 
+// SoftDev pd7
+// K27 -- Basic functions in JavaScript
+// 2023-04-04
+// --------------------------------------------------
+
+
+// as a duo...
+// pair programming style,
+// implement a fact and fib fxn
+
+
+//Do whatever you think is needed. Think: S I M P L E.   Think: S M A R T.
