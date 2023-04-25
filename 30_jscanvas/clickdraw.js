7ff08f4ba322f175fdd14e0ab49d05008f451f55
Kevin Xiao <78610793+FamousMrChair@users.noreply.github.com>
2023-04-25 00:22:44 -0400

    Update clickdraw.js

--- 30_jscanvas/clickdraw.js
+++ 30_jscanvas/clickdraw.js
@@ -1,48 +1,64 @@
-// retrieve node in DOM via ID
+// Team GASLIGHTING GATEKEEPING GIRLBOSSES :: Kevin Xiao + Aden Garbutt
+// SoftDev pd7
+// K30 -- canvas based JS drawing
+// 2023-04-24m
 
-console.log(document);
+// retrieve node in DOM via ID
 var c = document.getElementById("slate");
-console.log(document);
+var buttonToggle = document.getElementById("buttonToggle")
+var buttonClear = document.getElementById("buttonClear")
+
 //instantiatWe a CanvasRenderingContext2D object
 var ctx = c.getContext("2d");
 
-
 var mode = "rect";
-
-//var mouse = MouseEvent();
 
 //var toggleMode = function(e) {
 var toggleMode = (e) => {
-    console.log("toggling...");
+    //console.log("toggling...");
     if (mode == "rect") {
-        mode = "circle";
+      mode = "circle";
+      buttonToggle.innerHTML = "circle";
     }
     else {
-        mode = "rect";
+      mode = "rect";
+      buttonToggle.innerHTML = "rect";
     }
 }
 
+
 var drawRect = function(e) {
-    var mouseX = mouse.clientX;
-    var mouseY = mouse.clientY;
-    console.log("mouseclick registered at ", mouseX, mouseY);
+  var mouseX = e.clientX;
+  var mouseY = e.clientY;
+  ctx.fillRect(mouseX - 7, mouseY - 128, 25, 25)
+  console.log(`(${mouseX}, ${mouseY})`);
 }
+
 
 var drawCircle = (e) => {
-    console.log("mouseclick registered at ", mouseX, mouseY)
+  var mouseX = e.clientX
+  var mouseY = e.clientY
+  ctx.beginPath();
+  ctx.arc(mouseX - 7, mouseY - 128, 10, 0, 2 * Math.PI, true);
+  ctx.fill();
+  console.log(`(${mouseX}, ${mouseY})`);
 }
+
 
 var draw = (e) => {
-
+  if (mode == "rect") {
+    drawRect(e);
+  }
+  else {
+    drawCircle(e);
+  }
 }
+
 
 var wipeCanvas = () => {
-
+  ctx.clearRect(0, 0, 600, 600)
 }
-/*
-c.addEventListern("click", draw);
-var bToggler = document. ;
-bTogglerToggler. ;
-var clearB = ;
-clearB.;
-*/
+
+c.addEventListener("click", draw);
+buttonToggle.addEventListener("click", toggleMode);
+buttonClear.addEventListener("click", wipeCanvas);
